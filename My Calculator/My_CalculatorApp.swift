9542f6caf9070e2d0e0d67e2e04a5535b5426dd5
armstrongsouljah <armstrongsouljah@gmail.com>
2021-09-22 08:26:16 +0300

    Initial Commit

---
+++ My Calculator/My_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CalculatorApp.swift
+//  My Calculator
+//
+//  Created by Armstrong Muhwezi on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct My_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
